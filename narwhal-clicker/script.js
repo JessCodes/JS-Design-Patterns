bc825ba49693e5f49d458e4c5687432243f5ff6e
JessCodes <jessicafellison@gmail.com>
2017-03-25 21:42:56 -0700

    add html and javascript click counter

---
+++ narwhal-clicker/script.js
@@ -0,0 +1,14 @@
+let count = 0
+
+function narwhalClicks(){
+  count ++
+  clicks.textContent = count
+}
+
+const frank = document.getElementById('frank')
+
+frank.addEventListener('click', narwhalClicks)
+
+const clicks = document.getElementById('clicks')
+
+clicks.textContent = count
